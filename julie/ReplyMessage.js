8a3134be96797698226bfded6cf5ae57bc7ef175
Ali <84945779+Aces107@users.noreply.github.com>
2021-11-11 12:14:38 +0300

    Update ReplyMessage.js

--- julie/ReplyMessage.js
+++ julie/ReplyMessage.js
@@ -1,9 +1,9 @@
-/* Copyright (C) 2020 Yusuf Usta.
+/* Copyright (C) 2020 Ali ACES.
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-WhatsAsena - Yusuf Usta
+WhatsAsena - ALI ACES
 */
 
 const {MessageType, Presence, MessageOptions} = require('@adiwajshing/baileys');
